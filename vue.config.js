d1b0a237b214c18566dcf5f13f4cb83e14a97232
Bestian Tang <bestian@gmail.com>
2019-06-10 17:22:15 +0800

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  publicPath: '',
+  pluginOptions: {
+    cordovaPath: 'src-cordova'
+  },
+  chainWebpack: config => {
+      config.module.rule('pug')
+          .test(/\.pug$/)
+          .use('pug-html-loader')
+          .loader('pug-html-loader')
+          .end()
+  }
+}
