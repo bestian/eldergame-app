8558d846e4c3ab41c97c5c66ad5ca2206f2b5399
Bestian Tang <bestian@gmail.com>
2019-06-18 17:03:12 +0800

    停損點

---
+++ src-cordova/www/precache-manifest.acd0fd3690fd8f6f706cf4bf32a5e2f4.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "fonts/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "fonts/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "4b1a97323ed442b858bc",
+    "url": "css/chunk-vendors.5791b86a.css"
+  },
+  {
+    "revision": "bf71c16f47aa691e9ac07a5bb6fa8def",
+    "url": "img/認照片遊戲01.bf71c16f.jpg"
+  },
+  {
+    "revision": "aba47fd081cfd98da83e79818f4196a3",
+    "url": "img/認照片遊戲03.aba47fd0.jpg"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "fonts/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "fonts/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "img/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "fonts/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "fonts/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "fonts/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "fonts/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "fonts/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "6fe30ffa6cc1c4542fd1",
+    "url": "js/app.d37e7ddf.js"
+  },
+  {
+    "revision": "4b1a97323ed442b858bc",
+    "url": "js/chunk-vendors.aaf84622.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "fonts/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "fonts/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "fonts/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "img/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "img/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "img/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "ccb2778048ac903fde2dc41a456dba11",
+    "url": "img/th.ccb27780.jpg"
+  },
+  {
+    "revision": "9ad892b359eb51a4a5aab4158d576695",
+    "url": "img/認人遊戲01.9ad892b3.jpg"
+  },
+  {
+    "revision": "f72228fbddb561c85e42361e76f7856b",
+    "url": "img/旋轉遊戲01.f72228fb.jpg"
+  },
+  {
+    "revision": "d203c613160e61114b4e1967aaa99c0d",
+    "url": "img/aged1.d203c613.jpeg"
+  },
+  {
+    "revision": "cf5b88b7f7f34cd4b9637cdccc2d36d0",
+    "url": "index.html"
+  },
+  {
+    "revision": "6fe30ffa6cc1c4542fd1",
+    "url": "css/app.e97c7826.css"
+  }
+];
